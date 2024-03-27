939e85d4d037bcbf7db5817763979cd332ce887a
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-03-27 22:55:21 +0100

    chore: add success page

--- src/Pages/SuccessPage.jsx
+++ src/Pages/SuccessPage.jsx
@@ -1,16 +1,26 @@
 import iconSuccess from '../assets/images/icon-success.svg';
+import styles from '../styles/SuccessPage.module.css';
 
 const SuccessPage = () => {
   return (
-    <div>
-      {/* Icon Image */}
-      <div>
-        <img src={iconSuccess} alt="Success Icon" aria-label='Success Icon' />
-      </div>
+    <div className={styles.successContainer}>
       {/* <!-- Success message start --> */}
-      Thanks for subscribing! A confirmation email has been sent to
-      ash@loremcompany.com. Please open it and click the button inside to
-      confirm your subscription. Dismiss message
+
+      <div className={styles.successMessage}>
+      {/* Icon Image */}
+        <div className={styles.successIcon}>
+          <img src={iconSuccess} alt="Success Icon" aria-label="Success Icon" />
+        </div>
+
+        <div className={styles.subscribe}>
+          <h1>Thanks for subscribing!</h1>
+          <p>
+            A confirmation email has been sent to <strong>ash@loremcompany.com</strong>. Please
+            open it and click the button inside to confirm your subscription.
+          </p>{' '}
+        </div>
+      </div>
+      <button className={styles.dismissBtn}>Dismiss message</button>
       {/* <!-- Success message end --> */}
     </div>
   );
