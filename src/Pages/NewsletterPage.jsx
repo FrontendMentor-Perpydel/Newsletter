b6453ac2f29354131e6995616da82f5e30b77bd6
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-03-28 15:43:36 +0100

    fix: fix build issues on deployment

--- src/Pages/NewsletterPage.jsx
+++ src/Pages/NewsletterPage.jsx
@@ -1,4 +1,4 @@
-import FooterComponent from '../components/FooterComponent';
+import FooterComponent from '../components/layout/FooterComponent';
 import FormComponent from '../components/FormComponent';
 import ImageComponent from '../components/ImageComponent';
 import ListComponent from '../components/ListComponent';
@@ -6,28 +6,28 @@ import ListComponent from '../components/ListComponent';
 const NewsletterPage = () => {
   return (
     <>
-    <div className="main-container">
-      {/* Sign-up form start */}
-      <div className="form-left">
-        <h1 className="header">Stay updated!</h1>
-        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+      <div className="main-container">
+        {/* Sign-up form start */}
+        <div className="form-left">
+          <h1 className="header">Stay updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
 
-        {/* ListItems */}
-        <ListComponent />
+          {/* ListItems */}
+          <ListComponent />
 
-        {/* Form */}
-        <FormComponent />
+          {/* Form */}
+          <FormComponent />
+        </div>
+
+        {/* Image */}
+        <ImageComponent />
+        {/* <!-- Sign-up form end --> */}
       </div>
 
-      {/* Image */}
-      <ImageComponent />
-      {/* <!-- Sign-up form end --> */}
-    </div>
+      {/* Footer */}
+      <FooterComponent />
+    </>
+  );
+};
 
-    {/* Footer */}
-    <FooterComponent />
-  </>
-  )
-}
-
-export default NewsletterPage
+export default NewsletterPage;
