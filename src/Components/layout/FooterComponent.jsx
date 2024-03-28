e620703583be5638fb46afe2dd9272dfee0c447f
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-03-28 22:53:41 +0100

    docs: update README file

--- src/Components/layout/FooterComponent.jsx
+++ src/Components/layout/FooterComponent.jsx
@@ -2,15 +2,15 @@ import styles from '../../styles/FooterComponent.module.css';
 
 const FooterComponent = () => {
   return (
-    <div>
-      <div className={styles.attribution}>
+    <>
+      <footer className={styles.attribution}>
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
         . Coded by <a href="https://github.com/Perpy-del">Perpetual Meninwa</a>.
-      </div>
-    </div>
+      </footer>
+    </>
   );
 };
 
