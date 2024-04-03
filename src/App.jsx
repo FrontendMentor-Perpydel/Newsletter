fd89e6038e8375ffaaa0cc4ed682e1d61cf073ed
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-03 11:35:10 +0100

    fix: deploy issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Newsletter from './pages/Newsletter';
 import Success from './pages/Success';
 import NewsletterContextProvider from './context/NewsletterContext';
-import Footer from './components/layout/Footer';
+import Footer from './components/layout/Footer.jsx';
 
 function App() {
   return (
     <>
-      {/* <Router> */}
-      <NewsletterContextProvider>
-        <Routes>
-          <Route path="/" element={<Newsletter />} />
-          <Route path="/success" element={<Success />} />
-        </Routes>
-      </NewsletterContextProvider>
-      {/* </Router> */}
+      <Router>
+        <NewsletterContextProvider>
+          <Routes>
+            <Route path="/" element={<Newsletter />} />
+            <Route path="/success" element={<Success />} />
+          </Routes>
+        </NewsletterContextProvider>
+      </Router>
       <Footer />
     </>
   );
